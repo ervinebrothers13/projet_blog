b3fef9c76a3d492a42e82b7fa3317de7c6dad75a
ervinebrothers13 <143377433+ervinebrothers13@users.noreply.github.com>
2024-04-14 03:58:47 -1000

    Delete annuaireSimpleXML.php

--- annuaireSimpleXML.php
+++
@@ -1,7 +0,0 @@
-<?php
-$xml = simplexml_load_file("annuaire.xml");
-echo $xml->getName()."<br/><p>Listing :</p>";
-foreach($xml->children() as $child) {
-    echo $child->getName().": ".$child->nom."-".$child->prenom."<br/>";
-}
-?>
